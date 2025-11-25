18e863973746e664448174c7c0e9bba64df4bd10
Naboulibear <108967590+Naboulibear@users.noreply.github.com>
2025-11-25 01:43:51 +0000

    finshed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our", "another"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let tlds = [".com", ".net", ".org"];
+
+  for (const pronoun of pronouns) {
+    for (const adj of adjs) { 
+      for (const noun of nouns) {
+        for (const tld of tlds) {
+          console.log(pronoun + adj + noun + tld);
+
+        }
+      }
+    }
+  }
 };
